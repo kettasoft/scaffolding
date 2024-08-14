4fe97cd125eac5a593a37375e82323f993d2903f
Abdalrhman Emad Saad <a.emad@codeclouders.com>
2024-08-14 16:36:32 +0300

    Fix some bugs.

--- Modules/Accounts/Resources/views/sidebar.blade.php
+++ Modules/Accounts/Resources/views/sidebar.blade.php
@@ -1,5 +1,5 @@
 @component(layout('dashboard') . 'components.sidebarItem')
-    @slot('can', ['permission' => 'read_admins|read_parents'])
+    @slot('can', ['permission' => 'read_admins'])
     @slot('url', '#')
     @slot('name', trans('accounts::users.plural'))
     @slot('isActive', request()->routeIs('accounts*'))
@@ -10,13 +10,6 @@
         'url' => route('dashboard.admins.index'),
         'can' => ['permission' => 'read_admins'],
         'isActive' => request()->routeIs('*admins.*'),
-        'module' => 'Accounts',
-        ],
-        [
-        'name' => trans('accounts::parents.actions.list'),
-        'url' => route('dashboard.parents.index'),
-        'can' => ['permission' => 'read_parents'],
-        'isActive' => request()->routeIs('*parents.*'),
         'module' => 'Accounts',
         ],
         ])
